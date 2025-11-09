0b917ea4fde2be05bb7671203fa8b0e6122c7635
Marius Stroescu <stroescump@gmail.com>
2025-11-09 21:13:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Footer from './components/Footer'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Features from './components/Features'
+
+function App() {
+  return <div className='min-h-screen bg-slate-950 text-white overflow-hidden'>
+    <Navbar />
+    <Hero />
+    <Features />
+    <Pricing />
+    <Testimonials />
+    <Footer />
+  </div>
+}
+
+export default App
