e3eeb66360ee8770d80a35e917a8b7030a0bb5c9
Marius Stroescu <stroescump@gmail.com>
2025-11-11 16:10:00 +0100

    netlify deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [
     react({
       babel: {
@@ -11,5 +13,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    netlify()
   ],
 })
